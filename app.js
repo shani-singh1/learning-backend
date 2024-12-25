6bc3de3c12ec26b808b78e9513c4097afb4ebade
Shani Singh <teamdeveloperworld@gmail.com>
2024-12-25 15:19:57 +0530

    register/login api done

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './db/database.js';
+import userRouter from './routes/user.js'
 const app = express();
+
 dotenv.config()
 
 connectDB();
-
 const PORT = process.env.PORT || 3000
+
+app.use(express.json());
+
+app.use("/api/v1/user", userRouter);
 
 app.listen(PORT, ()=>{
     console.log( `Server listening at ${PORT}`)
---
+++ controllers/user.js
@@ -0,0 +1,70 @@
+import {User} from "../models/user.js"
+import bcrypt from 'bcrypt'
+
+export const register = async (req, res) =>{
+    try{
+        const {fullName, email, password} = req.body;
+
+        if(!fullName || !email || !password){
+            return res.status(403).json({
+                success: false,
+                message:"All fields are required !"
+            });
+        }
+        const user = await User.findOne({email});
+        if (user){
+            return res.status(403).json({
+                success: false,
+                message:"This email is already registered!"
+        }); 
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await User.create({
+        fullName,
+        email,
+        password: hashedPassword
+    })
+     return res.status(201).json({
+        success : true,
+        message : "User cretead successfully !"
+     })
+    }catch(error){
+        console.log(error);
+        
+    }
+}
+
+export const login = async (req, res) =>{
+    try{
+        const {email, password} = req.body;
+        if(!email || !password){
+            return res.status(403).json({
+                success: false,
+                message:"All fields are required !"
+            });
+        }
+        
+        const user = await User.findOne({email});
+        if (!user){
+            return res.status(403).json({
+                success: false,
+                message:"Incorrect email or password !"
+        }); 
+    }
+    const isPasswordMatched = bcrypt.compare(password, user.password);
+    if(!isPasswordMatched){
+        return res.json({
+            success:false,
+            message:"Email or password is Incorrect !"
+        })
+    }
+    return res.status(200).json({
+        success:true,
+        message:`Welcome 🎉 back ${user.fullName}`
+    })
+    }catch(error){
+        console.log(error);
+        
+    }
+}
